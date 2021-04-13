47bbc934aa51eabb97b56332bf7a995c8acfb236
Jesus Bueren <jesusbueren@gmail.com>
2021-04-13 11:10:14 +0200

    extrapolated transitions and medical expenses

--- SM_global_var.f90
+++ SM_global_var.f90
@@ -1,8 +1,8 @@
 module state_space_dim
     use nrtype
     implicit none
-    integer,parameter::first_age=25, last_age=98 !First and last age in the model
-    integer,parameter::T=(last_age-first_age+1)/2, T_R=(65-first_age+1)/2, T_50=(50-first_age+1)/2 !T: Model periods, T_R: time at retirement, T_50: period at age 50
+    integer,parameter::first_age_sm=25, last_age_sm=98 !First and last age in the model
+    integer,parameter::T=(last_age_sm-first_age_sm+1)/2, T_R=(65-first_age_sm+1)/2, T_50=(50-first_age_sm+1)/2 !T: Model periods, T_R: time at retirement, T_50: period at age 50
     integer,parameter::G_educ=3,G_PI=5,G_types=2,G_DF=1,G_h=2 !G_educ: education levels,G_PI: pension income,G_types: health behavior types,G_DF: discount factors,G_h: health types
     integer,parameter::G_nzz=5,G_nkk=50  !L_nzz: grid for the persistent shock 
     character(LEN=42)::path="C:\Users\jbueren\Google Drive\endo_health\"  
@@ -11,8 +11,8 @@ end module
 module var_first_step
     use nrtype;use state_space_dim
     implicit none
-    real(DP),dimension(G_h+1,G_h+1,T,G_types,G_educ)::H_fs=-9.0d0 !Health transition probabilities in the model
+    real(DP),dimension(G_h+1,G_h+1,T,G_types,G_educ)::H_sm=-9.0d0 !Health transition probabilities in the model
     real(DP),dimension(G_PI)::PI_grid=(/4.3d0,15.1d0,20.4d0,25.7d0,47.5d0/) ! Median of permanent income for each quantile
     real(DP),dimension(G_nzz,G_nzz)::Pi_m=-9.0d0 !Transition probabilities of the medical shock
-    real(DP),dimension(G_PI,T,G_h,G_nzz)::m_grid=-9.0d0 !Medical grid
+    real(DP),dimension(G_educ,T,G_h,G_nzz)::m_grid=-9.0d0 !Medical grid
 end module
--- load_first_step_p.f90
+++ load_first_step_p.f90
@@ -12,16 +12,16 @@ end subroutine
     
     
 subroutine load_transitions_retirees()
-    use global_var; use nrtype;use var_first_step
+    use global_var; use nrtype;use var_first_step; use state_space_dim
     implicit none
         integer,parameter::iterations=58640
         real(dp),dimension(clusters**2*covariates,iterations)::c_tr_all
         real(dp),dimension(covariates_habits*habits*types,iterations)::c_habits_all
         real(dp),dimension(clusters**2*covariates,1)::c_tr
         real(dp),dimension(covariates,clusters,clusters+1)::beta
-        real(DP),dimension(clusters,types,L_gender,L_educ)::init_cond
-        real(DP),dimension(clusters+1,clusters+1,generations,types,L_gender,L_educ)::H
-        real(DP),dimension(types,L_gender,L_educ,clusters+1)::LE
+        real(DP),dimension(clusters+1,clusters+1,T,types,L_gender,L_educ)::H
+        integer::t_l,h_l
+        real(DP),dimension(G_h+1,G_types,G_educ)::pr_sr
 
         open(unit=9,file=path_s//'c_tr.txt')
                 read(9,'(F20.8)') c_tr_all
@@ -32,25 +32,29 @@ subroutine load_transitions_retirees()
         beta=0.0d0
         beta(:,:,1:clusters)=reshape(c_tr,(/covariates,clusters,clusters/))
     
-        init_cond=0.0d0
-        init_cond(1,:,:,:)=1.0d0
+        call extrapolated_transitions(beta,H)
     
-        call transitions(beta,init_cond,H,LE)
-    
-        H_fs(:,:,T_50:T,:,:)=H(:,:,:,:,1,:)
-        
-
+        H_sm(:,:,1:T,:,:)=H(:,:,1:T,:,1,:)
+        !For individuals younger than fifty I assume that they don't die
+        do t_l=1,T_50-1
+            H_sm(1:G_h,G_h+1,T_50,:,:)=0.0d0
+            pr_sr=sum(H_sm(:,:,T_50,:,:),2)
+            do h_l=1,G_h
+                H_sm(:,h_l,t_l,:,:)=H_sm(:,h_l,T_50,:,:)/pr_sr
+            end do
+        end do
+!H_sm(1,1,:,2,1)
 end subroutine
     
 subroutine load_medical_retirees()
     use nrtype;use var_first_step;use state_space_dim
     implicit none
-    integer,parameter::K=8
+    integer,parameter::K=13
     real(DP),dimension(K+2)::data_csv
     real(DP),dimension(K,1)::X,beta
-    real(DP)::rho,sigma2_u,age,pi_income,h,z
+    real(DP)::rho,sigma2_u,age,h,z,d1,d2,d3
     real(DP),dimension(G_nzz,1)::mag_p,pr0_p
-    integer::t_l,pi_l,h_l,z_l
+    integer::t_l,e_l,h_l,z_l
     
     open(unit=9,file=path//'data\medical_exp.csv')
             read(9,*) data_csv
@@ -62,14 +66,23 @@ subroutine load_medical_retirees()
     
     call discretize_shocks(rho,sigma2_u,G_nzz,mag_p,pr0_p,Pi_m)
     
-    do t_l=T_R,T;do pi_l=1,G_PI;do h_l=1,G_h;do z_l=1,G_nzz
-        age=dble(first_age+(t_l-1)*2)
-        pi_income=PI_grid(pi_l)
+    do t_l=1,T;do e_l=1,G_educ;do h_l=1,G_h;do z_l=1,G_nzz
+        age=dble(first_age_sm+(t_l-1)*2)
+        d1=0.0d0
+        d2=0.0d0
+        d3=0.0d0
+        if (e_l==1) then
+            d1=1.0d0
+        elseif (e_l==2) then
+            d2=1.0d0
+        elseif (e_l==3) then
+            d3=1.0d0
+        end if
         h=dble(h_l-1)
         z=mag_p(z_l,1)
-        X(:,1)=(/pi_income, pi_income**2.0d0, age, age**2.0d0,h,h*pi_income,pi_income*age,1.0d0/)
-        m_grid(pi_l,t_l,h_l,z_l)=exp(sum(X(:,1)*beta(:,1))+z)
+        X(:,1)=(/d1,d2,d3,age,age**2.0d0,h,d1*h,d2*h,d3*h,d1*age,d2*age,d3*age,1.0d0/)
+        m_grid(e_l,t_l,h_l,z_l)=exp(sum(X(:,1)*beta(:,1))+z)
     end do;end do;end do;end do
-        
+       
 
 end subroutine
